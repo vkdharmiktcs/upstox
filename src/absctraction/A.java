48c9bdce35892e25a98b7d172b05d9a163ce22ec
vkdharmiktcs <118234717+vkdharmiktcs@users.noreply.github.com>
2022-11-15 08:28:22 +0530

    Update A.java

--- src/absctraction/A.java
+++ src/absctraction/A.java
@@ -3,6 +3,7 @@ package absctraction;
  abstract public class A {
 	public void m1() {//complete method 
 		System.out.println("Boss i am alredy completed");
+		System.out.println("vinay");
 	}
 	abstract public void m2();
 	abstract public void m3();
